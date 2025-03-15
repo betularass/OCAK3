51c7193b51748761169329eff9b008aa0ef96c38
betularass <betularas16@gmail.com>
2025-03-15 17:59:53 +0300

    OCAK3

---
+++ OCAK3/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+    // 1. SORU 
+
+//If else yapısı içerisinde koşulun sağlandığı kodlar if içerisinde yer alırken, koşulun sağlanmadığı kodlar ise else içerisinde yer alır. 
+//Bir"Eğer 2 saatten fazla kaldıysan ücret 50 TL, aksi halde 30 TL" gibi bir kural
+
+
+     // 2. SORU
+
+/*
+    
+    {
+        Console.Write("Bir sayı girin: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        if (sayi % 2 == 0)
+        {
+            Console.WriteLine("Girdiğiniz sayı çifttir.");
+        }
+        else
+        {
+            Console.WriteLine("Girdiğiniz sayı tektir.");
+        }
+    }
+*/
+
+
+       // 3.SORU
+
+   /*
+
+    {
+        Console.Write("Bir sayı girin: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        if (sayi > 0)
+        {
+            Console.WriteLine("Girdiğiniz sayı pozitiftir.");
+        }
+        else if (sayi < 0)
+        {
+            Console.WriteLine("Girdiğiniz sayı negatiftir.");
+        }
+        else
+        {
+            Console.WriteLine("Girdiğiniz sayı sıfırdır.");
+        }
+    }
+
+
+*/
+
+
